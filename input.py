e6b177642747c29c0e94113a454fe27acf00a9c8
jze-code <zulnajcouture2020@gmail.com>
2024-09-22 11:17:02 -0700

    Initial commit

---
+++ input.py
@@ -0,0 +1,8 @@
+import keyword
+name=input("Enter your name")
+print(name,"nice to meet you")
+age=int(input("Enter your age"))
+print("you are",age,"years old")
+temperature=float(input("Enter temperature in your area"))
+print("ok",temperature )
+print(keyword.kwlist)
